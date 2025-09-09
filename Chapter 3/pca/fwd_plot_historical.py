35ae6a8acd7e4cdc3e10fcbdbad3b25341b1cc48
joshuashing1 <joshuashingjunle@gmail.com>
2025-09-09 22:48:58 +0800

    edits to historical

--- Chapter 3/pca/fwd_plot_historical.py
+++ Chapter 3/pca/fwd_plot_historical.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data\GLC_fwd_curve_raw.csv")
+df = pd.read_csv("Chapter 3\data\GLC_fwd_curve_raw.csv")
 df = df.set_index("time")
 
 def parse_tenor(s: str) -> float:
@@ -12,27 +12,32 @@ def parse_tenor(s: str) -> float:
         return float(s[:-1]) / 12.0
     if s.endswith("Y"):
         return float(s[:-1])
-    # fallback: try raw numeric
     return float(s)
 
-# may change depending if I am using multiple currencies or not.
 tenor_years = pd.Series({c: parse_tenor(c) for c in df.columns})
 tenor_years = tenor_years.sort_values()
 df = df.loc[:, tenor_years.index]
 
-T = df.index.to_numpy()                      # time axis
-Tau = tenor_years.to_numpy()                 # tenor (years)
-X, Y = np.meshgrid(Tau, T)                   # shapes: [n_time, n_tenor]
-Z = df.to_numpy()                            # same shape as X/Y
+T = df.index.to_numpy()                      
+Tau = tenor_years.to_numpy()                 
+X, Y = np.meshgrid(Tau, T)                   
+Z = df.to_numpy()                            
 
-fig = plt.figure(figsize=(12, 6))
+DPI, W_PX, H_PX = 100, 1573, 750
+W_IN, H_IN = W_PX / DPI, H_PX / DPI
+
+fig = plt.figure(figsize=(W_IN, H_IN), dpi=DPI)
 ax = fig.add_subplot(111, projection="3d")
-
 surf = ax.plot_surface(X, Y, Z, cmap="inferno", linewidth=0, antialiased=True, alpha=0.95)
-
-ax.set_xlabel(r"Tenor $\tau$ (years)")
-ax.set_ylabel(r"Time $t$ (index)")
-ax.set_zlabel(r"Forward rate $f(t,\tau)$ [%]")
-fig.colorbar(surf, shrink=0.6, pad=0.08, label=r"$f(t,\tau)$ [%]")
-plt.tight_layout()
+ax.set_title("Historical forward rate", fontsize=37, fontweight="bold")
+ax.set_xlabel(r"Tenor $T$ (years)", fontsize=20, labelpad=15)
+ax.set_ylabel(r"Time $t$ (index)", fontsize=20, labelpad=13)
+ax.set_zlabel(r"Forward rate $f(t,T)$", fontsize=20, labelpad=13)
+ax.tick_params(axis="x", labelsize=16)
+ax.tick_params(axis="y", labelsize=16)
+ax.tick_params(axis="z", labelsize=16)
+fig.colorbar(surf, shrink=0.6, pad=0.08, label=r"$f(t,T)$")
+save_path = "historical_forward_surface.png"
+fig.tight_layout()
+fig.savefig(save_path, dpi=DPI)
 plt.show()
