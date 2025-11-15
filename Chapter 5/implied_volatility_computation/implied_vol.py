d1871a628c98ff1f6e34b555f8d4b91b4bd79843
joshuashing1 <joshuashingjunle@gmail.com>
2025-11-16 00:16:20 +0800

    edited the experiment for chapter 5

--- Chapter 5/implied_volatility_computation/implied_vol.py
+++ Chapter 5/implied_volatility_computation/implied_vol.py
@@ -7,22 +7,24 @@ from py_vollib.black.greeks.analytical import vega as vega_black
 def implied_vol(F0, K, T, annuity, market_price, flag="c", tol=1e-5, max_iter=100):
     sigma = 0.20  # initial guess
     for _ in range(max_iter):
-        core_price = black76(flag, F0, K, T, 0.0, sigma) # r=0 implies discounting via annuity
-        swaption_price = annuity * core_price                         
+        core_price = black76(flag, F0, K, T, 0.0, sigma)  # r=0 implies discounting via annuity
+        swaption_price = annuity * core_price
         diff = swaption_price - market_price
         if abs(diff) < tol:
             return sigma if sigma >= 0 else np.nan
-        v_core = vega_black(flag, F0, K, T, 0.0, sigma)      
-        v_sigma = annuity * v_core * 100.0                   
+
+        v_core = vega_black(flag, F0, K, T, 0.0, sigma)
+        v_sigma = annuity * v_core * 100.0
         if v_sigma == 0:
             break
+
         sigma -= diff / v_sigma
+
     return sigma if sigma >= 0 else np.nan
 
-T = 240.0 / 12.0 # option expiry (change accordingly)
+T = 300.0 / 12.0  # option expiry (change accordingly)
 
-pca_path   = r"Chapter 5\implied_volatility_computation\data\vae_20Y25Y_implied_volatility.csv"
-libor_path = r"Chapter 5\bloomberg_data\LIBOR_swaption_20Y25Y.csv"
+pca_path = r"Chapter 5\implied_volatility_computation\data\vae_20Y25Y_implied_volatility.csv"
 
 df_model = pd.read_csv(pca_path)
 df_model.columns = [c.strip() for c in df_model.columns]
@@ -39,20 +41,16 @@ df_model["implied_vol"] = df_model.apply(
     axis=1
 )
 
-df_mkt = pd.read_csv(libor_path)
-df_mkt.columns = [c.strip() for c in df_mkt.columns]       
-
-df = df_model.merge(df_mkt[["t", "mkt_implied_vol"]], on="t", how="inner")
-df = df.sort_values("t")
+df = df_model.sort_values("t")
 
 DPI = 100
 W_IN, H_IN = 1573 / DPI, 750 / DPI
 
-# change labels and path naming accordingly
 TICK_FS = 27
 fig, ax = plt.subplots(figsize=(W_IN, H_IN), dpi=DPI)
+
 ax.plot(df["t"], df["implied_vol"], marker="o", label="VAE implied vol")
-ax.plot(df["t"], df["mkt_implied_vol"], marker="x", linestyle="--", label="Market implied vol")
+
 ax.tick_params(axis="both", which="major", labelsize=TICK_FS)
 ax.tick_params(axis="both", which="minor", labelsize=TICK_FS)
 ax.xaxis.get_offset_text().set_size(TICK_FS)
@@ -62,7 +60,8 @@ ax.set_ylabel("Implied volatility", fontsize=32)
 ax.set_title("20Y × 25Y Swaption", fontsize=37, fontweight="bold", pad=12)
 ax.grid(True)
 ax.legend(fontsize=24)
+
 plt.tight_layout()
-out_path = r"vae_vs_mkt_20Y25Y.png"
+out_path = r"vae_vs_mkt_20Y25Y_model_only.png"
 plt.savefig(out_path, dpi=DPI, bbox_inches="tight")
 plt.show()
