a3ee06313904a4a59f2283979371268eb657bdef
joshuashing1 <joshuashingjunle@gmail.com>
2025-11-17 01:41:18 +0800

    deleted pre-processing for clarity in the script

--- Chapter 5/pre_processing/pre_processing.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv(r"Chapter 5\pre_processing\simulated_rates_input_csv\vae_simulated_fwd_rates.csv")
-df = df.drop(df.columns[[1, 2]], axis=1) # drop maturities 1M and 6M
-df.to_csv("vae_simulated_rates_selected.csv", index=False)
